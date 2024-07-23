4af93d8905c3e054588afad05d90cf03528c83d9
Berndoe <88336484+Berndoe@users.noreply.github.com>
2024-07-23 10:19:36 +0000

    Update home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -6,6 +6,10 @@ import 'package:capstone/services/booking_service.dart';
 import 'package:capstone/services/user_service.dart';
 import 'package:capstone/utils/constants.dart';
 import 'package:capstone/utils/helpers.dart';
+import 'package:capstone/views/profile_page.dart';
+import 'package:capstone/views/sustainable_practices.dart';
+import 'package:capstone/views/user_history.dart';
+import 'package:capstone/views/waste_collectors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:geolocator/geolocator.dart';
@@ -31,15 +35,37 @@ class _HomePageState extends State<HomePage> {
   final loc.Location _location = loc.Location();
   late ApiService _apiService;
   bool _isLocationEntered = false;
+  bool _isBookingInProgress = false;
+  bool _isDriverDetailsVisible = false;
   String userId = 'E5ZIRRES8yQBB0tYxYgf4xH561z1';
   final TextEditingController _locationController = TextEditingController();
   final TextEditingController _binNumberController =
       TextEditingController(text: '1');
   double? pickUpLat;
   double? pickUpLng;
+  double? driverLat;
+  double? driverLng;
+  String _distance = '';
+  String _duration = '';
+  String bookingId = '';
+  int _currentIndex = 0;
 
   Set<Marker> _markers = {};
   Set<Polyline> _polylines = {};
+
+  final List<Widget> _pages = [
+    HomePage(),
+    const WasteCollectors(),
+    const WastePractices(),
+    ProfilePage(profileType: ProfileType.user),
+    UserHistory(),
+  ];
+
+  void changePage(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   void _updateButtonState() {
     setState(() {
@@ -62,7 +88,6 @@ class _HomePageState extends State<HomePage> {
         ),
       );
 
-      // Get address from current location
       String address = await Helpers.getAddress(Position(
         latitude: currentLocation.latitude!,
         longitude: currentLocation.longitude!,
@@ -99,6 +124,92 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  Future<void> bookCollector() async {
+    var booking = await _bookingController.createBooking(
+      userId,
+      int.parse(_binNumberController.text),
+      pickUpLat!,
+      pickUpLng!,
+    );
+
+    if (booking.bookingId.isNotEmpty) {
+      var updatedBooking = await _apiService.post(
+        Constants.collectorsResponse,
+        {
+          "booking_id": booking.bookingId,
+          "collector_id": "az9PK12EPhMRz99Yvl8xHbKoxBH2"
+        },
+      );
+
+      if (updatedBooking['message'] == 'Booking accepted') {
+        bookingId = booking.bookingId;
+        LatLng? collectorLocation =
+            await Helpers.getCollectorLocation("az9PK12EPhMRz99Yvl8xHbKoxBH2");
+
+        if (collectorLocation != null) {
+          try {
+            var directions = await Helpers.getDirections(
+              LatLng(pickUpLat!, pickUpLng!),
+              collectorLocation,
+            );
+
+            PolylinePoints polylinePoints = PolylinePoints();
+            List<PointLatLng> locationPoints =
+                polylinePoints.decodePolyline(directions!.encodedPoints);
+
+            setState(() {
+              _distance = directions.distanceText;
+              _duration = directions.durationText;
+              _polylines.add(Polyline(
+                polylineId: const PolylineId('route'),
+                points: locationPoints
+                    .map((point) => LatLng(point.latitude, point.longitude))
+                    .toList(),
+                color: Colors.blue,
+                width: 5,
+              ));
+              _markers.add(Marker(
+                markerId: const MarkerId('collector'),
+                position: collectorLocation,
+                infoWindow: const InfoWindow(
+                  title: 'Collector',
+                  snippet: 'Your assigned collector',
+                ),
+              ));
+              _isDriverDetailsVisible = true;
+            });
+
+            _mapController?.animateCamera(
+              CameraUpdate.newLatLngBounds(
+                LatLngBounds(
+                  southwest: LatLng(
+                    locationPoints
+                        .map((point) => point.latitude)
+                        .reduce((a, b) => a < b ? a : b),
+                    locationPoints
+                        .map((point) => point.longitude)
+                        .reduce((a, b) => a < b ? a : b),
+                  ),
+                  northeast: LatLng(
+                    locationPoints
+                        .map((point) => point.latitude)
+                        .reduce((a, b) => a > b ? a : b),
+                    locationPoints
+                        .map((point) => point.longitude)
+                        .reduce((a, b) => a > b ? a : b),
+                  ),
+                ),
+                50,
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,6 +223,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.blue,
       ),
       drawer: Drawer(
+        backgroundColor: Colors.white,
         child: FutureBuilder<User>(
           future: _user,
           builder: (context, snapshot) {
@@ -178,57 +290,137 @@ class _HomePageState extends State<HomePage> {
               markers: _markers,
               polylines: _polylines,
             ),
-            Positioned(
-              bottom: 150,
-              left: 10,
-              right: 10,
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    child: placesAutoCompleteTextField(),
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    controller: _binNumberController,
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter number of bins',
-                      labelText: 'Number of bins',
-                      fillColor: Colors.white,
-                      filled: true,
-                      isDense: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        borderSide: BorderSide.none,
-                      ),
-                      contentPadding:
-                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            if (!_isBookingInProgress)
+              Positioned(
+                bottom: 150,
+                left: 10,
+                right: 10,
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: placesAutoCompleteTextField(),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      controller: _binNumberController,
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter number of bins',
+                        labelText: 'Number of bins',
+                        fillColor: Colors.white,
+                        filled: true,
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            if (_isBookingInProgress)
+              Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () async {
+                    // Handle cancel booking
+                    setState(() {
+                      _isBookingInProgress = false;
+                      _isDriverDetailsVisible = false;
+                      _markers.clear();
+                      _polylines.clear();
+                    });
+                  },
+                  child: const Text(
+                    'Cancel Booking',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
             Positioned(
               bottom: 20,
               left: 20,
               right: 20,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  backgroundColor:
-                      _isLocationEntered ? Colors.blue : Colors.grey,
+                  backgroundColor: _isLocationEntered
+                      ? (_isBookingInProgress ? Colors.red : Colors.blue)
+                      : Colors.grey,
                 ),
                 onPressed: () async {
-                  _isLocationEntered ? bookCollector() : null;
+                  if (_isLocationEntered) {
+                    if (_isBookingInProgress) {
+                      await _bookingController.deleteBooking(bookingId);
+                      setState(() {
+                        _isBookingInProgress = false;
+                        _isDriverDetailsVisible = false;
+                        _markers.clear();
+                        _polylines.clear();
+                      });
+                    } else {
+                      showBookingDetailsSheet();
+                    }
+                  }
                 },
                 child: Text(
-                  'Request Waste Collector',
+                  _isBookingInProgress
+                      ? 'Cancel Booking'
+                      : 'Request Waste Collector',
                   style: TextStyle(
-                    color: _isLocationEntered ? Colors.white : Colors.black,
+                    color: _isLocationEntered
+                        ? (_isBookingInProgress ? Colors.white : Colors.white)
+                        : Colors.black,
                   ),
                 ),
               ),
             ),
+            if (_isDriverDetailsVisible)
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          'Driver Location: ${driverLat ?? ''}, ${driverLng ?? ''}'),
+                      Text('Distance: $_distance'),
+                      Text('Duration: $_duration'),
+                      const SizedBox(height: 10),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isBookingInProgress = false;
+                            _isDriverDetailsVisible = false;
+                            _markers.clear();
+                            _polylines.clear();
+                            _bookingController.deleteBooking(bookingId);
+                          });
+                        },
+                        child: const Text(
+                          'Cancel Booking',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       ),
@@ -260,9 +452,7 @@ class _HomePageState extends State<HomePage> {
             backgroundColor: Colors.blue,
           ),
         ],
-        onTap: (index) {
-          // Handle navigation here
-        },
+        onTap: changePage,
       ),
     );
   }
@@ -302,74 +492,40 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  void bookCollector() async {
-    var booking = await _bookingController.createBooking(
-      userId,
-      int.parse(_binNumberController.text),
-      pickUpLat!,
-      pickUpLng!,
-    );
-
-    if (booking.bookingId.isNotEmpty) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('Booking Created'),
-            content: const Text('Your booking has been created successfully.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
+  void showBookingDetailsSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Confirm Booking',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Text('Number of bins: ${_binNumberController.text}'),
+              Text('Location: ${_locationController.text}'),
+              // Replace with actual price calculation logic
+              const Text('Price: \$20'),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  Navigator.of(context)
+                      .pop(); // Close the booking details sheet
+                  setState(() {
+                    _isBookingInProgress = true;
+                  });
+                  await bookCollector(); // Proceed with booking
                 },
-                child: const Text('OK'),
+                child: const Text('Confirm'),
               ),
             ],
-          );
-        },
-      );
-
-      PolylinePoints polylinePoints = PolylinePoints();
-      var updatedBooking = await _apiService.post(
-        Constants.collectorsResponse,
-        {
-          "booking_id": booking.bookingId,
-          "collector_id": "az9PK12EPhMRz99Yvl8xHbKoxBH2"
-        },
-      );
-
-      if (updatedBooking['message'] == 'Booking accepted') {
-        LatLng? collectorLocation =
-            await Helpers.getCollectorLocation("az9PK12EPhMRz99Yvl8xHbKoxBH2");
-
-        setState(() {
-          _markers.add(Marker(
-            markerId: const MarkerId('collector'),
-            position: collectorLocation!,
-          ));
-        });
-
-        if (pickUpLat != null && pickUpLng != null) {
-          var directions = await Helpers.getDirections(
-            LatLng(pickUpLat!, pickUpLng!),
-            collectorLocation!,
-          );
-
-          List<PointLatLng> locationPoints =
-              polylinePoints.decodePolyline(directions!.encodedPoints);
-
-          setState(() {
-            _polylines.add(Polyline(
-              polylineId: const PolylineId('route'),
-              points: locationPoints
-                  .map((point) => LatLng(point.latitude, point.longitude))
-                  .toList(),
-              color: Colors.blue,
-              width: 5,
-            ));
-          });
-        }
-      }
-    }
+          ),
+        );
+      },
+    );
   }
 }
